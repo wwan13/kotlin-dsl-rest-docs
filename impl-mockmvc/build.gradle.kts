39399858e811589ebe0df3fdf2b6c4dec4d6473c
wwan13 <wwan13@naver.com>
2024-07-06 01:05:02 +0900

    chore : remove useless dependency

--- impl-mockmvc/build.gradle.kts
+++ impl-mockmvc/build.gradle.kts
@@ -3,6 +3,5 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    implementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     implementation("com.epages:restdocs-api-spec-mockmvc:0.16.0")
 }
