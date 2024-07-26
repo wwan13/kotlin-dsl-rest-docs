b0db5673f7119f4ae5d1cdb87793c0b52c1e58b1
wwan13 <wwan13@naver.com>
2024-07-26 23:30:16 +0900

    feat : Register objectmapper DateTime module

--- impl-mockmvc/build.gradle.kts
+++ impl-mockmvc/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     api(project(":api"))
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
     implementation("com.epages:restdocs-api-spec-mockmvc:0.16.0")
 }
--- impl-mockmvc/src/main/kotlin/io/wwan13/implmockmvc/util/JsonSerializer.kt
+++ impl-mockmvc/src/main/kotlin/io/wwan13/implmockmvc/util/JsonSerializer.kt
@@ -1,10 +1,11 @@
 package io.wwan13.implmockmvc.util
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 object JsonSerializer {
 
-    private val objectMapper = jacksonObjectMapper()
+    private val objectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
     fun serialize(value: Any): String {
         return objectMapper.writeValueAsString(value)
