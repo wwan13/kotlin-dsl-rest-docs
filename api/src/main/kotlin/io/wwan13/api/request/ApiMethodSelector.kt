9b87a246e06b0baebc8ebd0bcbdd5385c70e2308
wwan13 <wwan13@naver.com>
2024-07-26 23:30:29 +0900

    feat : Add default build action

--- api/src/main/kotlin/io/wwan13/api/request/ApiMethodSelector.kt
+++ api/src/main/kotlin/io/wwan13/api/request/ApiMethodSelector.kt
@@ -7,30 +7,30 @@ interface ApiMethodSelector {
     fun get(
         path: String,
         vararg pathParameters: Any = arrayOf(),
-        buildAction: ApiRequestBuilder.() -> Unit
+        buildAction: ApiRequestBuilder.() -> Unit = { }
     ): ResultActions
 
     fun post(
         path: String,
         vararg pathParameters: Any = arrayOf(),
-        buildAction: ApiRequestBuilder.() -> Unit
+        buildAction: ApiRequestBuilder.() -> Unit = { }
     ): ResultActions
 
     fun put(
         path: String,
         vararg pathParameters: Any = arrayOf(),
-        buildAction: ApiRequestBuilder.() -> Unit
+        buildAction: ApiRequestBuilder.() -> Unit = { }
     ): ResultActions
 
     fun patch(
         path: String,
         vararg pathParameters: Any = arrayOf(),
-        buildAction: ApiRequestBuilder.() -> Unit
+        buildAction: ApiRequestBuilder.() -> Unit = { }
     ): ResultActions
 
     fun delete(
         path: String,
         vararg pathParameters: Any = arrayOf(),
-        buildAction: ApiRequestBuilder.() -> Unit
+        buildAction: ApiRequestBuilder.() -> Unit = { }
     ): ResultActions
 }
