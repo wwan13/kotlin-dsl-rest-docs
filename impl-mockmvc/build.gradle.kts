7ae86495cddfbf3bf26a65a158d4d492378f6d9a
wwan13 <wwan13@naver.com>
2024-10-24 16:16:44 +0900

    chore : upgrade epages.mockmvc 0.16 to 0.17

--- impl-mockmvc/build.gradle.kts
+++ impl-mockmvc/build.gradle.kts
@@ -1,8 +1,9 @@
 dependencies {
-    api(project(":api"))
+    compileOnly(project(":api"))
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
-    implementation("com.epages:restdocs-api-spec-mockmvc:0.16.0")
+    implementation("com.epages:restdocs-api-spec-mockmvc:0.17.1")
+    implementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.0")
 }
